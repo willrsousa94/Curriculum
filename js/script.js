8647594686751e4e592a24b5a5d6127f25336526
William Rodrigues de Sousa <rm78369@fiap.com.br>
2019-06-04 17:44:35 -0300

    pequenas alterações

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 $("#sobre").click(mostrarSobreMim);
 $("#experiencia").click(mostrarExperiencia);
+$("#cursos").click(mostrarCursos);
 
 function mostrarSobreMim(){
     $(".inactive").hide()
-    $(".sobre-mim").show(200);
+    $(".sobre-mim").show(600);
 }
 
 function mostrarExperiencia(){
     $(".inactive").hide();
-    $(".experiencia").show(200);
+    $(".experiencia").show(600);
+}
+
+function mostrarCursos(){
+    $(".inactive").hide();
+    $(".info-cursos").show(600);
 }
