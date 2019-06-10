b6c7f2d6675f1376e7768f17c8b99a42e9a8cd31
William Rodrigues de Sousa <rm78369@fiap.com.br>
2019-06-10 16:17:05 -0300

    Layout upgrade

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $("#sobre").click(mostrarSobreMim);
 $("#experiencia").click(mostrarExperiencia);
 $("#cursos").click(mostrarCursos);
+$("#portfolio").click(mostrarPortfolio);
 
+ /* $(document).ready(function(){
+    $(".inicio").show();
+}); */
 function mostrarSobreMim(){
     $(".inactive").hide()
     $(".sobre-mim").show(600);
@@ -15,4 +19,9 @@ function mostrarExperiencia(){
 function mostrarCursos(){
     $(".inactive").hide();
     $(".info-cursos").show(600);
+}
+
+function mostrarPortfolio(){
+    $(".inactive").hide();
+    $(".info-portfolio").show(600);
 }
